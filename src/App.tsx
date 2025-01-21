c7714faf64b2a27b0e926111a2110e3b023751d9
Jaewon <wellbird@naver.com>
2025-01-21 20:29:39 +0900

    feat : boardList 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { useState } from 'react';
 import { appContainer, board, buttons } from './App.css';
+import BoardList from './components/BoardList/BoardList';
 
 function App() {
+  const [activeBoardId, setActiveBoardId] = useState('board-0');
   return (
     <div className={appContainer}>
+      <BoardList
+        activeBoardId={activeBoardId}
+        setActiveBoardId={setActiveBoardId}
+      />
       <div className={board}></div>
       <div className={buttons}>
         <button>이 게시판 삭제하기기</button>
--- src/components/BoardList/BoardList.tsx
+++ src/components/BoardList/BoardList.tsx
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useTypedSelector } from '../../hooks/redux';
+import SideForm from './SideForm/SideForm';
+import { FiPlusCircle } from 'react-icons/fi';
+import {
+  addButton,
+  addSection,
+  boardItem,
+  boardItemActive,
+  container,
+  title,
+} from './BoardList.css';
+import clsx from 'clsx';
 
-const BoardList = () => {
-  return <div>BoardList</div>;
+type TBoardListProps = {
+  activeBoardId: string;
+  setActiveBoardId: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const BoardList: React.FC<TBoardListProps> = ({
+  activeBoardId,
+  setActiveBoardId,
+}) => {
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const { boardArray } = useTypedSelector((state) => state.boards);
+
+  return (
+    <div className={container}>
+      <div className={title}>게시판 : </div>
+      {boardArray.map((board, index) => (
+        <div
+          key={board.boardId}
+          onClick={() => setActiveBoardId(boardArray[index].boardId)} // 여기 왜 board.boardId가 아니라 저렇게 넣지? 나중에 다 만들면 테스트 할 것
+          className={clsx(
+            {
+              [boardItemActive]:
+                boardArray.findIndex((b) => b.boardId === activeBoardId) ===
+                index,
+            },
+            {
+              [boardItem]:
+                boardArray.findIndex((b) => b.boardId === activeBoardId) !==
+                index,
+            }
+          )}
+        >
+          <div>{board.boardName}</div>
+        </div>
+      ))}
+      <div className={addSection}>
+        {isFormOpen ? (
+          <SideForm setIsFormOpen={setIsFormOpen} />
+        ) : (
+          <FiPlusCircle
+            className={addButton}
+            onClick={() => setIsFormOpen(!isFormOpen)}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default BoardList;
