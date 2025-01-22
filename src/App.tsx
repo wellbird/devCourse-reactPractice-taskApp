caab63620f4557078a73963c3d719aea1501ad15
Jaewon <wellbird@naver.com>
2025-01-22 20:57:30 +0900

    feat : loggerModal, logItem 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { useState } from 'react';
-import { appContainer, board, buttons } from './App.css';
+import {
+  appContainer,
+  board,
+  buttons,
+  deleteBoardButton,
+  loggerButton,
+} from './App.css';
 import BoardList from './components/BoardList/BoardList';
 import ListsContainer from './components/ListsContainer/ListsContainer';
 import { useTypedSelector } from './hooks/redux';
 import EditModal from './components/EditModal/EditModal';
+import LoggerModal from './components/LoggerModal/LoggerModal';
 
 function App() {
+  const [isLoggerOpen, setIsLoggerOpen] = useState(false);
   const [activeBoardId, setActiveBoardId] = useState('board-0');
   const modalActive = useTypedSelector((state) => state.boards.modalActive);
   const boards = useTypedSelector((state) => state.boards.boardArray);
@@ -16,6 +24,7 @@ function App() {
 
   return (
     <div className={appContainer}>
+      {isLoggerOpen ? <LoggerModal setIsLoggerOpen={setIsLoggerOpen} /> : null}
       {modalActive ? <EditModal /> : null}
       {/**이 부분 && 로 해도 될라나 나중에 테스트 */}
       <BoardList
@@ -27,8 +36,10 @@ function App() {
         {/**이 부분도 현재 상태에 있는 activeBoardId가 더 명확할 것 같은데 왜 이렇게 했는지 나중에 테스트 */}
       </div>
       <div className={buttons}>
-        <button>이 게시판 삭제하기기</button>
-        <button></button>
+        <button className={deleteBoardButton}>이 게시판 삭제하기</button>
+        <button className={loggerButton} onClick={() => setIsLoggerOpen(true)}>
+          {isLoggerOpen ? '활동 목록 숨기기' : '활동 목록 보이기'}
+        </button>
       </div>
     </div>
   );
--- src/components/LoggerModal/LogItem/LogItem.tsx
+++ src/components/LoggerModal/LogItem/LogItem.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { ILogItem } from '../../../types';
+import { BsFillPersonFill } from 'react-icons/bs';
+import { author, date, logItemWrap, message } from './LogItem.css';
 
-const LogItem = () => {
-  return <div>LogItem</div>;
+type TLogItemProps = {
+  logItem: ILogItem;
+};
+
+const LogItem: React.FC<TLogItemProps> = ({ logItem }) => {
+  const timeOffset = new Date(Date.now() - Number(logItem.logTimestamp));
+  const showOffsetTime = `
+  ${timeOffset.getMinutes() > 0 ? `${timeOffset.getMinutes()}m` : ''}
+  ${timeOffset.getSeconds() > 0 ? `${timeOffset.getSeconds()}s ago` : ''}
+  ${timeOffset.getSeconds() === 0 ? 'just now' : ''}
+  `;
+  return (
+    <div className={logItemWrap}>
+      <div className={author}>
+        <BsFillPersonFill />
+        {logItem.logAuthor}
+      </div>
+      <div className={message}>{logItem.logMessage}</div>
+      <div className={date}>{showOffsetTime}</div>
+    </div>
+  );
 };
 
 export default LogItem;
